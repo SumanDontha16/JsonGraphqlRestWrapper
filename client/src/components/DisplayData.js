91af11028769a460134dd7e068beaa8a272ab703
Suman Dontha <dontha.suman@gmail.com>
2022-09-26 11:49:39 +0100

    Update the components to ES6 standards.

--- client/src/components/DisplayData.js
+++ client/src/components/DisplayData.js
@@ -2,8 +2,8 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import { GET_ALL_ALBUMS } from "../graphql/Query";
 
-function DisplayData() {
-  const { loading, error, data } = useQuery(GET_ALL_ALBUMS);
+const DisplayData = () => {
+    const { loading, error, data: {albums} = [] } = useQuery(GET_ALL_ALBUMS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
@@ -11,13 +11,13 @@ function DisplayData() {
   return (
     <div>
       <h2>ALBUMS LIST</h2>
-      {data.albums.map((album) => (
+      {albums.map((album) => (
         <div key={album.id}>
           <p>{album.title}</p>
         </div>
       ))}
     </div>
   );
-}
+ }
 
 export default DisplayData;
--- client/src/components/GetUsers.js
+++ client/src/components/GetUsers.js
@@ -2,8 +2,8 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import { LOAD_USERS } from "../graphql/Query";
 
-function GetUsers() {
-  const { loading, error, data } = useQuery(LOAD_USERS);
+const GetUsers = () => {
+  const { loading, error, data: { users } = [] } = useQuery(LOAD_USERS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
@@ -11,7 +11,7 @@ function GetUsers() {
   return (
     <div>
       <h2>USERS LIST</h2>
-      {data.users.map((user) => (
+      {users.map((user) => (
         <div key={user.id} className="container-wrap">
           <p>{user.name}</p>
           <p>{user.email}</p>
@@ -20,6 +20,6 @@ function GetUsers() {
       ))}
     </div>
   );
-}
+ } 
 
 export default GetUsers;
